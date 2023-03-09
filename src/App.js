55655dc16c9f370595d387914b1d4bd6707e42f5
Gaurav <gvnsaxena@gmail.com>
2023-03-09 10:55:38 +0000

    Updating App.js with click handler and productId

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
@@ -6,10 +6,15 @@ import Autocomplete from "./Autocomplete";
 import ProductDetail from "./ProductDetail";
 
 function App() {
+  const [selectedSuggestion, setSelectedSuggestion] = useState(0)
+
+  function handleClick(productId) {
+    setSelectedSuggestion(productId)
+  }
   return (
     <div className="App">
-      <Autocomplete />
-      <ProductDetail productId={null} />
+      <Autocomplete handleClick={handleClick}/>
+      <ProductDetail productId={selectedSuggestion} />
     </div>
   );
 }
