00182648de5db44ec34c50a32d0cd788891da021
Gaurav <gvnsaxena@gmail.com>
2023-03-09 11:04:39 +0000

    Auto-Complete Component 1) Creating list items for queried letters 2) Adding handlers to manage events on list items 3) Adding Unit tests 4) making component accessible

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,19 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
-import { fetchSuggestions } from "./utils/api";
+import { fetchSuggestions } from "./utils/api"
 
-import "./Autocomplete.css";
+import "./Autocomplete.css"
 
-function Autocomplete() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+const Autocomplete = ({ handleClick }) => {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [suggestions, setSuggestions] = useState([])
+  const [debouncedValue, setDebouncedValue] = useState('')
 
   useEffect(() => {
-    fetchSuggestions(searchTerm).then((_suggestions) =>
-      setSuggestions(_suggestions)
-    );
-  }, [searchTerm]);
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(searchTerm)
+    }, 500)
 
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [searchTerm])
+  
+  useEffect(() => {
+    if(debouncedValue) {
+      fetchSuggestions(debouncedValue).then((_suggestions) => 
+        setSuggestions(_suggestions)
+      ).catch((error) => {
+        console.error(`Error in fetching Suggestion List: ${error}`)
+      })
+    } else {
+      setSuggestions([])
+    }
+  }, [debouncedValue])
+
+  const handleOnClick = (event) => {
+    if (event.key === 'Enter' || event.type === 'click') {
+      handleClick(event.target.id)
+      setSuggestions([])
+      setSearchTerm('')
+      setDebouncedValue('')
+    }
+  }
   return (
     <div className="search-container">
       <input
@@ -23,9 +48,19 @@ function Autocomplete() {
         placeholder="Search for a product"
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      {/* TODO: render search suggestions */}
+      <ul className="list-menu">
+        {suggestions ? suggestions.slice(0,10).map((suggestion) => {
+          return( 
+            <li tabIndex="0" key={suggestion.id} onClick={handleOnClick} 
+            id={suggestion.id}
+            onKeyDown={handleOnClick}>
+              {suggestion.title}
+            </li>
+          )
+        }) : null}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default Autocomplete;
+export default Autocomplete
--- src/Autocomplete.test.js
+++ src/Autocomplete.test.js
@@ -1,16 +1,130 @@
-import React from "react";
-import {render, screen} from '@testing-library/react'
+import React from "react"
+import {render, screen, fireEvent, waitFor} from '@testing-library/react'
 
-import Autocomplete from "./Autocomplete";
-
-jest.mock("./utils/api");
+import Autocomplete from "./Autocomplete"
 
 describe("Autocomplete", () => {
-  it("renders correctly", () => {
-    render(<Autocomplete />);
+  let fetchSpy = jest.spyOn(global, 'fetch')
+  const handleClick = jest.fn()
 
+  beforeEach(() => {
+    fetchSpy.mockResolvedValue({
+      json: async () => [
+        { id: 1, title: 'Title 1' },
+      ],
+    })
+    render(<Autocomplete handleClick={handleClick}/>)
+  })
+
+  afterEach(jest.resetAllMocks)
+
+  it("renders correctly", () => {
     const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
     
-    expect(input).toBeInTheDocument();
-  });
-});
+    expect(input).toBeInTheDocument()
+  })
+  
+  it("renders no suggestions when Input is not entered", async () => {
+    await waitFor(() => {
+      const searchResults = screen.queryAllByRole('listitem')
+      expect(searchResults.length).toBe(0)
+    })
+  })
+
+  it("renders suggestions in list when Input is entered ", async () => {
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const searchResults = screen.getAllByRole('listitem')
+      expect(searchResults.length).toBe(1)
+    })
+  })
+
+  it("renders maximum of 10 suggestions when Input is entered", async () => {
+    const list = new Array(20).fill(1)
+    
+    fetchSpy.mockResolvedValue({
+      json: async () => 
+        list.map((item, index) => ({
+          id: index,
+          title: "Title"
+        })
+      )
+    })
+
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const searchResults = screen.getAllByRole('listitem')
+      expect(searchResults.length).toBe(10)
+    })
+  })
+
+  it("should select the clicked suggestion Item from suggestions list", async () => {
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const searchResults = screen.getByText('Title 1')
+      fireEvent.click(searchResults)
+      expect(handleClick).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it("renders no suggestions list and clears Input box when one suggestion is selected from the list", async () => {
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const searchResults = screen.getByText('Title 1')
+      fireEvent.click(searchResults)
+
+      const listSearchResults = screen.queryAllByRole('listitem')
+      expect(listSearchResults.length).toBe(0)
+    })
+    
+    expect(input.value).toBe('')
+  })
+
+  it('should debounce the network request', async () => {
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+    
+    fireEvent.change(input, { target: { value: 'a' } })
+    fireEvent.change(input, { target: { value: 'ab' } })
+    fireEvent.change(input, { target: { value: 'abc' } })
+    
+    await waitFor(() => expect(fetchSpy).toHaveBeenCalledTimes(1))
+  })
+
+  it("should select the sugesstion Item from the suggestions list on Enter Keypress", async () => {
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const searchResults = screen.getByText('Title 1')
+      fireEvent.keyDown(searchResults, { key: 'Enter' })
+      expect(handleClick).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it("should catch error while featching Suggestion list", async () => {
+    fetchSpy.mockRejectedValue(new Error('fetching problem'))
+    const consoleSpy = jest.spyOn(console, 'error').mockReturnValue(() => {});
+
+    const input = screen.getByRole('textbox', {placeholder: /search for a product/i})
+
+    fireEvent.change(input, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(consoleSpy).toHaveBeenCalledWith('Error in fetching Suggestion List: Error: fetching problem')
+    })
+  })
+})
